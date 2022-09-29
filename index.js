0b54f407bc48f773f50966f23c583702070c34e6
Eno <eno.meta@gmx.fr>
2022-09-29 14:36:14 +0200

    Contact function created - Toggle function added to the form and to few links

---
+++ index.js
@@ -0,0 +1,40 @@
+// template_8xxygbh
+// service_uq3yrup
+// gwWv5AXkV2U7NPQvQ
+
+function contact(event) {
+    // Prevent the default action
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
+    emailjs
+        .sendForm(
+            'service_uq3yrup',
+            'template_8xxygbh',
+            event.target,
+            'gwWv5AXkV2U7NPQvQ'
+        ).then(() => {
+            loading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible";
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly on eno.meta@gmx.fr"
+            );
+        })
+
+    setTimeout(() => {
+    }, 1000);
+}
+
+
+let isModalOpen = false;
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open"
+}
